5796742fdd9c4a454a0cdc4a1255da686402d761
js <jareks@com.pl>
2024-10-07 12:53:21 +0200

    Make GUI

---
+++ Vgr1Paszport/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Vgr1Paszport;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Submit(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
