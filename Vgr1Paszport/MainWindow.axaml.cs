a3a864a0fef8fa79e56a75b82503a4cb6a5335eb
js <jareks@com.pl>
2024-10-07 13:31:48 +0200

    Add logic for images

--- Vgr1Paszport/MainWindow.axaml.cs
+++ Vgr1Paszport/MainWindow.axaml.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 
 namespace Vgr1Paszport;
 
@@ -12,6 +16,57 @@ public partial class MainWindow : Window
 
     private void Submit(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        var numText = NumberTextBox.Text ?? "";
+        var nameText = NameTextBox.Text ?? "";
+        var lastnameText = LastnameTextBox.Text ?? "";
+        var eyeColorText = "";
+        
+        
+        bool radioButtonChecked;
+
+        if (beerColor.IsChecked ?? false)
+            eyeColorText = "piwny";
+        else if (greenColor.IsChecked ?? false)
+            eyeColorText = "zielony";
+        else if (blueColor.IsChecked ?? false)
+            eyeColorText = "niebieski";
+
+        if (numText != "" && nameText != "" && lastnameText != "" && eyeColorText != "")
+        {
+            DialogWindow dialog = new();
+            dialog.ShowDialog(this);
+        }
+        else
+        {
+            DialogWindow dialog = new();
+            dialog.ShowDialog(this);
+        }
+    }
+
+    private void NumberTextBox_OnLostFocus(object? sender, RoutedEventArgs e)
+    {
+        var num = NumberTextBox.Text ?? "brak";
+
+        var fingerprintName = $"{num}-odciski.jpg";
+        var pictureName = $"{num}-zdjecie.jpg";
+
+        for (;;)
+        {
+            try
+            {
+                using var streamFingerprint = AssetLoader.Open(new Uri($"avares://Vgr1Paszport/Assets/{fingerprintName}"));
+                FingerprintImage.Source = new Bitmap(streamFingerprint);
+
+                using var streamPicture = AssetLoader.Open(new Uri($"avares://Vgr1Paszport/Assets/{pictureName}"));
+                PictureImage.Source = new Bitmap(streamPicture);
+                break;
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                fingerprintName = $"brak-odciski.jpg";
+                pictureName = $"brak-zdjecie.jpg";
+            }
+        }
+        
     }
 }
--- Vgr1Paszport/obj/Debug/net8.0/Vgr1Paszport.AssemblyInfo.cs
+++ Vgr1Paszport/obj/Debug/net8.0/Vgr1Paszport.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vgr1Paszport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5796742fdd9c4a454a0cdc4a1255da686402d761")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vgr1Paszport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vgr1Paszport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
